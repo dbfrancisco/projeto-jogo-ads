5d03dd6beb626e1be078eaa2ad9aa99f8b0476ac
Francisco Anderson Pereira da Costa <20232ADS.CNT0038@ifma.edu>
2025-05-30 21:31:08 -0300

    commit

--- estrutura-do-projeto/js/game-menu.js
+++ estrutura-do-projeto/js/game-menu.js
@@ -1,23 +1,34 @@
 function mostrarSubmenu(tipo) {
-    document.getElementById("submenu").style.display = "block";
-    let titulo = "";
-  
-    switch (tipo) {
-      case "memoria": titulo = "Jogo da Memória"; break;
-      case "associacao": titulo = "Jogo de Associação"; break;
-      case "quiz": titulo = "Quiz"; break;
-    }
-  
-    document.getElementById("submenu-title").innerText = titulo;
+  document.getElementById("submenu").style.display = "block";
+  let titulo = "";
+
+  switch (tipo) {
+    case "memoria": titulo = "Jogo da Memória"; break;
+    case "associacao": titulo = "Jogo de Associação"; break;
+    case "quiz": titulo = "Quiz"; break;
   }
-  
-  function voltar() {
-    document.getElementById("submenu").style.display = "none";
-    document.getElementById("submenu-title").innerText = "";
+
+  document.getElementById("submenu-title").innerText = titulo;
+}
+
+function voltar() {
+  document.getElementById("submenu").style.display = "none";
+  document.getElementById("submenu-title").innerText = "";
+}
+
+function iniciarJogo(tipo) {
+  const rotas = {
+    xadrez: "jogos/xadrez.html",
+    cartas: "jogos/cartas.html",
+    geografia: "jogos/quiz-geografia.html",
+    historia: "jogos/quiz-historia.html",
+    animais: "jogos/associacao-animais.html",
+    cores: "jogos/associacao-cores.html"
+  };
+
+  if (rotas[tipo]) {
+    window.location.href = rotas[tipo];
+  } else {
+    alert("Jogo ainda não implementado.");
   }
-  
-  function iniciarJogo(tipo) {
-    alert("Carregar: " + tipo + " (iremos usar JSON ou lógica própria)");
-    // Aqui futuramente você pode redirecionar ou carregar a lógica dos jogos
-  }
-  
+}
